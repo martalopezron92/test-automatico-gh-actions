7ab4067558101e32cf3438cf36386839f682a8d5
Marta92 <mlopron829@g.educaand.es>
2025-11-24 17:03:45 +0100

    añado historico de ejecuciones al readme

--- src/update_readme.py
+++ src/update_readme.py
@@ -1,4 +1,5 @@
 import subprocess
+from datetime import datetime
 
 def run_tests():
     try:
@@ -8,19 +9,23 @@ def run_tests():
         return "❌ Tests fallidos"
 
 def update_readme(status: str):
-    with open("README.md", "r", encoding="utf-8") as f:
-        lines = f.readlines()
+    # with open("README.md", "r", encoding="utf-8") as f:
+    #     lines = f.readlines()
 
-    new_lines = []
-    for line in lines:
-        new_lines.append(line)
-        if line.strip() == "## Estado de los tests":
-            new_lines.append(status + "\n")
-            break
+    # new_lines = []
+    # for line in lines:
+    #     new_lines.append(line)
+    #     if line.strip() == "## Estado de los tests":
+    #         new_lines.append(status + "\n")
+    #         break
 
-    with open("README.md", "w", encoding="utf-8") as f:
-        f.writelines(new_lines)
+    # with open("README.md", "w", encoding="utf-8") as f:
+    #     f.writelines(new_lines)
 
+    with open("README.md", "a") as f:
+        ahora = datetime.now()
+        f.write(ahora + ": " + status + "\n")
+    
 if __name__ == "__main__":
     status = run_tests()
     update_readme(status)
